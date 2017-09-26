5b40d0ffdca0c8f274b91b4d1a39cd67d821ae62
Lorenzo Campanis <info@lcampanis.com>
2017-09-26 21:01:32 +0300

    added meteor for immutable

--- src/immutable.js
+++ src/immutable.js
@@ -1,8 +1,8 @@
-import { connect } from 'react-redux'
+import { connect } from 'react-redux-meteor'
 import { LoadingBar } from './loading_bar'
 
 const mapImmutableStateToProps = state => ({
   loading: state.get('loadingBar'),
 })
 
-export default connect(mapImmutableStateToProps)(LoadingBar)
+export default connect(null, mapImmutableStateToProps)(LoadingBar)
